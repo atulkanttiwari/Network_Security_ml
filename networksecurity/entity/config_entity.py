3a61668bdd3a0ba5ae6763ab35430d2b7ac2a19c
Atul Kant Tiwari <atulkanttiwari5@gmail.com>
2026-02-13 23:20:46 +0530

    cloud s3 bucket

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -12,7 +12,7 @@ class TrainingPipelineConfig:
         self.artifact_name=training_pipeline.ARTIFACT_DIR
         self.artifact_dir=os.path.join(self.artifact_name,timestamp)
         self.timestamp: str=timestamp
-        
+        self.model_dir = training_pipeline.SAVED_MODEL_DIR
         
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
